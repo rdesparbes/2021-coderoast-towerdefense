0042fcea08f0df5ab7699d3a32a51bee86ffb81f
rdesparbes <desparbes.romain@hotmail.fr>
2022-11-18 08:16:46 +0100

    Added kwargs to the default tower factories

--- adapted/tower_defense/entities/default/towers.py
+++ adapted/tower_defense/entities/default/towers.py
@@ -26,112 +26,114 @@ from tower_defense.entities.orientation_strategies import (
 TowerMapping = Dict[str, ITowerFactory]
 
 
+_TOWER_FACTORIES = [
+    TowerFactory(
+        tower_name="Arrow Shooter",
+        model_name="ArrowShooterTower",
+        tower_cost=150,
+        projectile_factory=ProjectileFactory(
+            projectile_name="arrow",
+            projectile_stats=ProjectileStats(
+                speed=Up(20.0),
+                range=Up(10.5, 11.5),
+                hitbox_radius=Up(1.0),
+                range_sensitive=Up(True),
+                effects=UpgradableList(
+                    [
+                        DamageEffect(damage=Up(10, 120)),
+                        StunEffect(duration=Up(0.25)),
+                    ]
+                ),
+            ),
+            movement_strategy=constant_angle_movement_strategy,
+            hit_strategy=near_enough_hit_strategy,
+        ),
+        tower_stats=TowerStats(
+            shots_per_second=Up(1, 1, 3),
+            projectile_count=Up(1),
+            upgrade_cost=Up(50, 100),
+        ),
+        orientation_strategy=target_orientation_strategy,
+    ),
+    TowerFactory(
+        tower_name="Bullet Shooter",
+        model_name="BulletShooterTower",
+        tower_cost=150,
+        projectile_factory=ProjectileFactory(
+            projectile_name="bullet",
+            projectile_stats=ProjectileStats(
+                speed=Up(10),
+                range=Up(6.5),
+                hitbox_radius=Up(0.5),
+                range_sensitive=Up(False),
+                effects=UpgradableList([DamageEffect(damage=Up(5))]),
+            ),
+            movement_strategy=tracking_movement_strategy,
+            hit_strategy=tracking_hit_strategy,
+        ),
+        tower_stats=TowerStats(
+            shots_per_second=Up(4),
+            projectile_count=Up(1),
+        ),
+        orientation_strategy=null_orientation_strategy,
+    ),
+    TowerFactory(
+        tower_name="Power Tower",
+        model_name="PowerTower",
+        tower_cost=150,
+        projectile_factory=ProjectileFactory(
+            projectile_name="powerShot",
+            projectile_stats=ProjectileStats(
+                speed=Up(20.0),
+                range=Up(8.5),
+                hitbox_radius=Up(0.25),
+                range_sensitive=Up(False),
+                effects=UpgradableList(
+                    [
+                        DamageEffect(damage=Up(1)),
+                        SlowEffect(factor=Up(3.0), duration=Up(0.1)),
+                    ]
+                ),
+            ),
+            movement_strategy=tracking_movement_strategy,
+            hit_strategy=tracking_hit_strategy,
+        ),
+        tower_stats=TowerStats(
+            shots_per_second=Up(10),
+            projectile_count=Up(1),
+        ),
+        orientation_strategy=null_orientation_strategy,
+    ),
+    TowerFactory(
+        tower_name="Tack Tower",
+        model_name="TackTower",
+        tower_cost=200,
+        projectile_factory=ProjectileFactory(
+            projectile_name="arrow",
+            projectile_stats=ProjectileStats(
+                speed=Up(20.0),
+                range=Up(5.0),
+                hitbox_radius=Up(1.0),
+                range_sensitive=Up(True),
+                effects=UpgradableList(
+                    [
+                        DamageEffect(damage=Up(10)),
+                        StunEffect(duration=Up(0.25)),
+                    ]
+                ),
+            ),
+            movement_strategy=constant_angle_movement_strategy,
+            hit_strategy=near_enough_hit_strategy,
+        ),
+        tower_stats=TowerStats(
+            shots_per_second=Up(1),
+            projectile_count=Up(8),
+        ),
+        orientation_strategy=concentric_orientation_strategy,
+    ),
+]
+
+
 TOWER_MAPPING: TowerMapping = {
-    tower_factory.get_name(): tower_factory
-    for tower_factory in (
-        TowerFactory(
-            "Arrow Shooter",
-            "ArrowShooterTower",
-            150,
-            ProjectileFactory(
-                "arrow",
-                ProjectileStats(
-                    speed=Up(20.0),
-                    range=Up(10.5, 11.5),
-                    hitbox_radius=Up(1.0),
-                    range_sensitive=Up(True),
-                    effects=UpgradableList(
-                        [
-                            DamageEffect(damage=Up(10, 120)),
-                            StunEffect(duration=Up(0.25)),
-                        ]
-                    ),
-                ),
-                constant_angle_movement_strategy,
-                near_enough_hit_strategy,
-            ),
-            TowerStats(
-                shots_per_second=Up(1, 1, 3),
-                projectile_count=Up(1),
-                upgrade_cost=Up(50, 100),
-            ),
-            target_orientation_strategy,
-        ),
-        TowerFactory(
-            "Bullet Shooter",
-            "BulletShooterTower",
-            150,
-            ProjectileFactory(
-                "bullet",
-                ProjectileStats(
-                    speed=Up(10),
-                    range=Up(6.5),
-                    hitbox_radius=Up(0.5),
-                    range_sensitive=Up(False),
-                    effects=UpgradableList([DamageEffect(damage=Up(5))]),
-                ),
-                tracking_movement_strategy,
-                tracking_hit_strategy,
-            ),
-            TowerStats(
-                shots_per_second=Up(4),
-                projectile_count=Up(1),
-            ),
-            null_orientation_strategy,
-        ),
-        TowerFactory(
-            "Power Tower",
-            "PowerTower",
-            150,
-            ProjectileFactory(
-                "powerShot",
-                ProjectileStats(
-                    speed=Up(20.0),
-                    range=Up(8.5),
-                    hitbox_radius=Up(0.25),
-                    range_sensitive=Up(False),
-                    effects=UpgradableList(
-                        [
-                            DamageEffect(damage=Up(1)),
-                            SlowEffect(factor=Up(3.0), duration=Up(0.1)),
-                        ]
-                    ),
-                ),
-                tracking_movement_strategy,
-                tracking_hit_strategy,
-            ),
-            TowerStats(
-                shots_per_second=Up(10),
-                projectile_count=Up(1),
-            ),
-            null_orientation_strategy,
-        ),
-        TowerFactory(
-            "Tack Tower",
-            "TackTower",
-            200,
-            ProjectileFactory(
-                "arrow",
-                ProjectileStats(
-                    speed=Up(20.0),
-                    range=Up(5.0),
-                    hitbox_radius=Up(1.0),
-                    range_sensitive=Up(True),
-                    effects=UpgradableList(
-                        [
-                            DamageEffect(damage=Up(10)),
-                            StunEffect(duration=Up(0.25)),
-                        ]
-                    ),
-                ),
-                constant_angle_movement_strategy,
-                near_enough_hit_strategy,
-            ),
-            TowerStats(
-                shots_per_second=Up(1),
-                projectile_count=Up(8),
-            ),
-            concentric_orientation_strategy,
-        ),
-    )
+    tower_factory.get_name(): tower_factory for tower_factory in _TOWER_FACTORIES
 }
